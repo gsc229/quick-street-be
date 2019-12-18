e7ee2f7022efed7b65de880c22c0233137ef1b71
dominique maack <dominiquemaack@gmail.com>
2019-12-17 17:29:25 -0700

    test routes and request handlers working in postman

--- controllers/vendors.js
+++ controllers/vendors.js
@@ -1,1 +1,19 @@
-// we will make queries for the vendors here
+exports.getAllVendors = (req, res, next) => {
+    res.send('List of all vendors');
+};
+
+exports.getVendor = (req, res, next) => {
+    res.send('Single vendor');
+};
+
+exports.createVendor = (req, res, next) => {
+    res.send('create new vendor');
+}
+
+exports.updateVendor = (req, res, next) => {
+    res.send('update vendor');
+}
+
+exports.deleteVendor = (req, res, next) => {
+    res.send('delete vendor');
+}
--- routes/vendors.js
+++ routes/vendors.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const {
+    getAllVendors,
+    getVendor,
+    createVendor,
+    updateVendor,
+    deleteVendor
+} = require('../controllers/vendors');
 
-// router.route('/')
+router.route('/').get(getAllVendors).post(createVendor);
 
+router.route('/:id').get(getVendor).put(updateVendor).delete(deleteVendor);
 
 
 module.exports = router;

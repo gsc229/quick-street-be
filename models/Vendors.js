74588ee234a35015040e92c5114df889dd0bb3c0
Luis Ocasio <luisocasio03@gmail.com>
2019-12-19 12:24:52 -0500

    Encrypt password using bcrypt

--- models/Vendors.js
+++ models/Vendors.js
@@ -100,4 +100,19 @@ const Vendor_Schema = new mongoose.Schema({
   }
 });
 
+// Encrypt password using bcrypt
+UserSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+// Match user entered password to hashed password in database
+UserSchema.methods.matchPassword = async function(enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
 module.exports = mongoose.model('Vendors', Vendor_Schema);

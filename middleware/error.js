2733595b44f8dd9b15ac1f3f148b2e95addc58af
dominique maack <dominiquemaack@gmail.com>
2019-12-19 15:36:33 -0700

    errorHandling middleware tested with vendor routes and working properly

--- middleware/error.js
+++ middleware/error.js
@@ -16,7 +16,7 @@ const errorHandler = (err, req, res, next) => {
     error = new ErrorResponse(message, 404)
   }
 
-
+// Mongoose duplicate key
   if (err.code === 11000) {
     const message = 'Duplicate field value entered';
     error = new ErrorResponse(message, 400);

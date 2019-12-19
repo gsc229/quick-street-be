c9c4a47aa15c48c0cf9c998422f3e703c3eaa060
dominique maack <dominiquemaack@gmail.com>
2019-12-19 15:45:32 -0700

    mode vendor controller DRY w/ asyncHandler middleware

--- controllers/vendors.js
+++ controllers/vendors.js
@@ -1,15 +1,14 @@
 const Vendor = require('../models/Vendors');
 const ErrorResponse = require('../utils/errorResponse'); // allows custom error responses
-const asyncHandler = require('../middleware/async');
+const asyncHandler = require('../middleware/async'); // keeps code DRY
 
 exports.getAllVendors = asyncHandler(async (req, res, next) => {
-    try {
         const vendors = await Vendor.find();
 
         res.status(200).json({ success: true, data: vendors })
-    } catch {
+
         res.status(400).json({ success: false })
-    }
+
 });
 
 exports.getVendor = asyncHandler(async (req, res, next) => {
@@ -30,7 +29,6 @@ exports.createVendor = asyncHandler(async (req, res, next) => {
 });
 
 exports.updateVendor = asyncHandler(async (req, res, next) => {
-    try {
         const vendor = await Vendor.findByIdAndUpdate(req.params.id, req.body, {
             new: true,
             runValidators: true
@@ -41,11 +39,7 @@ exports.updateVendor = asyncHandler(async (req, res, next) => {
                 new ErrorResponse(`Vendor not found with the id of ${req.params.id}`, 404)
             );
         }
-
         res.status(200).json({ success: true, data: vendor });
-    } catch (err) {
-        next(err);
-    }
 });
 
 exports.deleteVendor = asyncHandler(async (req, res, next) => {

5ce36908fc2798745d455fa571fc891e5050dc12
dominique maack <dominiquemaack@gmail.com>
2019-12-20 10:03:39 -0700

    working on GET vendors by zipcode and distance

--- controllers/vendors.js
+++ controllers/vendors.js
@@ -1,6 +1,7 @@
 const Vendor = require('../models/Vendors');
 const ErrorResponse = require('../utils/errorResponse'); // allows custom error responses
 const asyncHandler = require('../middleware/async'); // keeps code DRY
+const geocoder = require('../utils/geocoder');
 
 exports.getAllVendors = asyncHandler(async (req, res, next) => {
         const vendors = await Vendor.find();
@@ -28,6 +29,9 @@ exports.createVendor = asyncHandler(async (req, res, next) => {
     res.status(201).json({ success: true, data: vendor });
 });
 
+// @desc    Update single vendor
+// @route   PUT /api/v1.0/vendors/:id
+// @access  Private
 exports.updateVendor = asyncHandler(async (req, res, next) => {
         const vendor = await Vendor.findByIdAndUpdate(req.params.id, req.body, {
             new: true,
@@ -42,6 +46,9 @@ exports.updateVendor = asyncHandler(async (req, res, next) => {
         res.status(200).json({ success: true, data: vendor });
 });
 
+// @desc    Delete single vendor
+// @route   DELETE /api/v1.0/vendors/:id
+// @access  Private
 exports.deleteVendor = asyncHandler(async (req, res, next) => {
     const vendor = await Vendor.findById(req.params.id)
     if(!vendor) {
@@ -53,4 +60,18 @@ exports.deleteVendor = asyncHandler(async (req, res, next) => {
     vendor.remove();
 
     res.status(200).json({ success: true, data: {} });
+});
+
+// @desc    Get vendors within a radius
+// @route   GET /api/v1.0/vendors/raduis/:zipcode/:distance
+// @access 
+exports.getVendorsInRadius = asyncHandler(async (req, res, next) => {
+    const { zipcode, distance } = rew.params;
+
+    // Get lat/lng from geocoder
+    const loc = await geocoder.geocode(zipcode);
+    const lat = loc[0].latitude;
+    const lng = loc[0].latitude;
+
+    // Calc radius using radians
 });

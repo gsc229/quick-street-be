83e2dbab7e5f3e3ef0cc84fe75af26133f8a776c
dominique maack <dominiquemaack@gmail.com>
2019-12-20 16:46:02 -0700

    GET single product by its id

--- controllers/products.js
+++ controllers/products.js
@@ -14,7 +14,10 @@ exports.getAllProducts = asyncHandler(async (req, res, next) => {
             vendor: req.params.vendorId
         })
     } else {
-        query = Product.find();
+        query = Product.find().populate({
+            path: 'vendor',
+            select: 'business_name description'
+        });
     }
 
     const products = await query;
@@ -23,5 +26,27 @@ exports.getAllProducts = asyncHandler(async (req, res, next) => {
         success: true,
         count: products.length,
         data: products
+    });
+});
+
+// @desc    Get a single products
+// @route   GET /api/v1.0/products/:id
+// @access  Public
+exports.getProduct = asyncHandler(async (req, res, next) => {
+   const product = await Product.findById(req.params.id).populate({
+       path: 'vendor',
+       select: 'business_name description'
+   });
+
+   if(!product) {
+       return next(new ErrorResponse(`No product with the id of ${req.params.id}`),
+       404
+       );
+   }
+
+    res.status(200).json({
+        success: true,
+        count: product.length,
+        data: product
     });
 });
--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const {
-    getAllProducts
+    getAllProducts,
+    getProduct
 } = require('../controllers/products');
 
 const Product = require('../models/Product');
@@ -8,5 +9,7 @@ const Product = require('../models/Product');
 const router = express.Router({ mergeParams: true }); //merging the URL files
 
 router.route('/').get(getAllProducts);
+
+router.route('/:id').get(getProduct)
 
 module.exports = router;

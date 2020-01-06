8801bc7a2be46a10a81f059614a8d1e806dd1ce1
Greg <gsc229@gmail.com>
2020-01-06 16:17:13 -0500

    Product_Image schema built

---
+++ models/ProductImage.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const Product_Image = new mongoose.Schema({
+
+  public_id: String,
+  version: Number,
+  signature: String,
+  width: Number,
+  height: Number,
+  format: String,
+  resource_type: String,
+  created_at: String,
+  tags: [String],
+  bytes: Number,
+  type: String,
+  etag: String,
+  placeholder: Boolean,
+  url: String,
+  secure_url: String,
+  access_mode: String,
+  original_filename: String,
+  path: String,
+  thumbnail_url: String,
+  product: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Product',
+    required: true
+  }
+
+});
+
+module.exports = mongoose.model('ProductImage', Product_Image);

ae7f756e8b085664c31d15d9688ec616f097f322
dominique maack <dominiquemaack@gmail.com>
2020-01-09 10:57:44 -0700

    added sendEmail function for reseting password, installed nodemailer dependency, added sendEmail utility file

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const crypto = require('crypto');
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
+const sendEmail = require('../utils/sendEmail');
 const Vendor = require('../models/Vendor');
 
 // @desc      Register vendor
@@ -92,10 +93,34 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
 
   console.log('reset token', resetToken);
 
+  // Create reset URL
+  const resetUrl = `${req.protocol}://${req.get('host')}/api/v1.0/resetpassword/${resetToken}`;
+
+  const message = `You are receiving this email because you or (or someone else) has requested the reset of a password. Please make a PUT request to: \n\n ${resetUrl}`; //include the front end link here 
+
+
+  try {
+    await sendEmail({
+      email: vendor.email,
+      subject: 'Password reset token',
+      message 
+    });
+
+    res.status(200).json({ success: true, data: 'Email sent' })
+  } catch (err) {
+    console.log(err);
+    vendor.resetPasswordToken = undefined;
+    vendor.resetPasswordExpire = undefined;
+
+    await vendor.save({ validateBeforeSave: false })
+
+    return next(new ErrorResponse('Email could not be sent', 500));
+  }
   res.status(200).json({
     success: true,
     data: vendor
   });
+
 });
 
 // Get token from model, create cookie and send response

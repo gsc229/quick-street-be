c3424e1123ded508e481415d5f3681fab4765e81
dominique maack <dominiquemaack@gmail.com>
2020-01-14 14:51:25 -0700

    cleaning up code and put notes on the routes that will require axiosWithAuth

--- routes/auth.js
+++ routes/auth.js
@@ -15,10 +15,10 @@ const { protect } = require('../middleware/auth');
 
 router.post('/register', register);
 router.post('/login', login);
-router.get('/logout', protect, logout);
-router.get('/me', protect, getMe);
-router.put('/updatedetails', protect, updateDetails);
-router.put('/updatepassword', protect, updatePassword);
+router.get('/logout', protect, logout); //  GET /api/v1/auth/logout
+router.get('/me', protect, getMe); // POST /api/v1/auth/me
+router.put('/updatedetails', protect, updateDetails); // PUT /api/v1/auth/updatedetails
+router.put('/updatepassword', protect, updatePassword); // PUT /api/v1/auth/updatepassword
 router.post('/forgotpassword', forgotPassword);
 router.put('/resetpassword/:resettoken', resetPassword);
 
--- routes/posts.js
+++ routes/posts.js
@@ -15,11 +15,18 @@ const { protect } = require('../middleware/auth');
 
 const router = express.Router({ mergeParams: true });
 
-router.route('/').get(advancedResults(Posts, {
-    path: 'vendor',
-    select: 'title description date'
-}), getAllPosts).post(protect, addPost);
+router
+    .route('/')
+        .get(advancedResults(Posts, {
+            path: 'vendor',
+            select: 'title description date'
+        }), getAllPosts)
+        .post(protect, addPost); // POST /api/v1.0/vendors/:vendorId/posts
 
-router.route('/:id').get(getPost).put(protect, updatePost).delete(protect, deletePost);
+router
+    .route('/:id')
+        .get(getPost)
+        .put(protect, updatePost) // PUT /api/v1.0/post/:id
+        .delete(protect, deletePost); // DELETE /api/v1.0/vendors/:vendorId/posts
 
 module.exports = router;

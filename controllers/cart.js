2e2fabdac52eb4e5416fcf26f8f9af74a1579f74
cjgodfather <chaoji@Chaos-MacBook-Pro.local>
2020-01-24 06:27:56 +0800

    added update shopping cart

--- controllers/cart.js
+++ controllers/cart.js
@@ -1,88 +1,115 @@
-const Cart = require('../models/Cart');
-const Customer = require('../models/Customer');
-const Product = require('../models/Product');
-const asyncHandler = require('../middleware/async');
-const ErrorResponse = require('../utils/errorResponse');
-
-
+const Cart = require("../models/Cart");
+const Customer = require("../models/Customer");
+const Product = require("../models/Product");
+const asyncHandler = require("../middleware/async");
+const ErrorResponse = require("../utils/errorResponse");
 
 // @desc    Get cart
 // @route   GET /api/v1.0/customers/:customerId/cart
 // @access  Public
 exports.getCart = asyncHandler(async (req, res, next) => {
-    console.log('customerId', req.params.customerId)
+  console.log("customerId", req.params.customerId);
 
-    const cart = await Cart.findOne({
-        owner: req.params.customerId,
-    })
-    .populate('items.item', 'name price');
+  const cart = await Cart.findOne({
+    owner: req.params.customerId
+  }).populate("items.item", "name price");
 
-    if(!cart) {
-        return next(new ErrorResponse(`No customer with id ${req.params.customerId} owns this cart`, 404))
-    }
+  if (!cart) {
+    return next(
+      new ErrorResponse(
+        `No customer with id ${req.params.customerId} owns this cart`,
+        404
+      )
+    );
+  }
 
-    res.status(200).json({ success: true, data: cart });
+  res.status(200).json({ success: true, data: cart });
 });
-
 
 // @desc    Create cart to customer
 // @route   POST /api/v1.0/customers/:customerId/cart
 // @access  Public
 exports.addCart = asyncHandler(async (req, res, next) => {
-    req.body.owner = req.params.customerId;
-    console.log('Creating new cart from customerId', req.body.owner);
+  req.body.owner = req.params.customerId;
+  console.log("Creating new cart from customerId", req.body.owner);
 
-    const customer = await Customer.findById(req.params.customerId);
+  const customer = await Customer.findById(req.params.customerId);
 
-    if(!customer) {
-        return next( new ErrorResponse(`No customer with the id of ${req.params.customerId}`, 404))
-    }
+  if (!customer) {
+    return next(
+      new ErrorResponse(
+        `No customer with the id of ${req.params.customerId}`,
+        404
+      )
+    );
+  }
 
-    const cart = await Cart.create(req.body);
+  const cart = await Cart.create(req.body);
 
-    res.status(200).json({
-        success: true,
-        data: cart
-    });
-})
+  res.status(200).json({
+    success: true,
+    data: cart
+  });
+});
 
 // @desc    Add products to cart
 // @route   POST /api/v1.0/:customerId/addtocart
 // @access  Public
 exports.addItem = (req, res, next) => {
+  Cart.findOne({ owner: req.params.customerId }, function(err, cart) {
+    cart.items.push({
+      item: req.body.productId,
+      price: parseFloat(req.body.price),
+      quantity: parseInt(req.body.quantity)
+    });
 
-    Cart.findOne({ owner: req.params.customerId}, function(err, cart) {
-        cart.items.push({
-            item: req.body.productId,
-            price: parseFloat(req.body.price),
-            quantity: parseInt(req.body.quantity)
-        })
+    cart.total = (cart.total + parseFloat(req.body.price)).toFixed(2);
 
-        cart.total = (cart.total + parseFloat(req.body.price)).toFixed(2);
+    cart.save();
+  });
 
-        cart.save();
-    
-        })
+  res
+    .status(200)
+    .json({ success: true, message: "Product was added to your cart" });
+};
 
-res.status(200).json({ success: true, message: 'Product was added to your cart'})
-        
-}
+// @desc    update products to cart
+// @route   PUT /api/v1.0/:customerId/addtocart
+// @access  Public
+
+exports.updateCart = (req, res, next) => {
+  Cart.findOne({ owner: req.params.customerId }, function(err, cart) {
+    cart.items = [];
+    cart.items.push({
+      item: req.body.productId,
+      price: parseFloat(req.body.price),
+      quantity: parseInt(req.body.quantity)
+    });
+
+    cart.total = (cart.total + parseFloat(req.body.price)).toFixed(2);
+
+    cart.save();
+  });
+
+  res
+    .status(200)
+    .json({ success: true, message: "Cart was updated with new items" });
+};
 
 // @desc    Delete cart
 // @route   DELETE /api/v1.0/customers/:customerId/cart
 // @access  Private
 exports.deleteCart = asyncHandler(async (req, res, next) => {
-    const cart = await Cart.findOneAndDelete(req.body.items)
+  const cart = await Cart.findOneAndDelete(req.body.items);
 
-    if(!cart) {
-        return next(new ErrorResponse(`Cart not found`, 404))
-    }
+  if (!cart) {
+    return next(new ErrorResponse(`Cart not found`, 404));
+  }
 
-    cart.remove();
+  cart.remove();
 
-    res.status(200).json({
-        success: true,
-        data: {}
-    })
-})
-
+  res.status(200).json({
+    success: true,
+    data: {}
+  });
+});

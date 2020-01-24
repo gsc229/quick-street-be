3111b7566e876a324533c773d3858723cb9136fe
cjgodfather <chaoji@Chaos-MacBook-Pro.local>
2020-01-24 12:24:57 +0800

    added cart routes

--- routes/cart.js
+++ routes/cart.js
@@ -1,22 +1,24 @@
-const express = require('express');
+const express = require("express");
 const {
-    getCart,
-    addCart,
-    addItem,
-    deleteCart
-} = require('../controllers/cart');
-const Cart = require('../models/Cart');
+  getCart,
+  addCart,
+  addItem,
+  updateCart,
+  deleteCart
+} = require("../controllers/cart");
+const Cart = require("../models/Cart");
 
-const advancedResults = require('../middleware/advancedResults');
+const advancedResults = require("../middleware/advancedResults");
 
 const router = express.Router({ mergeParams: true }); //merging the URL files
 
-router  
-    .route('/')
-        .get(getCart)
-        .post(addCart)
-        .delete(deleteCart);
 router
-    .route('/addtocart')
-        .post(addItem)
+  .route("/")
+  .get(getCart)
+  .post(addCart)
+  .delete(deleteCart);
+router
+  .route("/addtocart")
+  .post(addItem)
+  .put(updateCart);
 module.exports = router;

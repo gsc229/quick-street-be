31f5a3548842111f96850b3e4681b2e559f78747
Greg <gsc229@gmail.com>
2020-05-15 03:43:48 -0400

    commented out console.logs

--- middleware/advancedResults.js
+++ middleware/advancedResults.js
@@ -4,28 +4,28 @@ const advancedResults = (model, populate) => async (req, res, next) => {
 
   // Check to see if a query exists with Object.entries  
   if (!Object.entries(req.query).length && (req.params.vendorId || req.params.productId) && model.modelName !== "ProductImage") {
-    console.log(`No query! Sending to controller`.red)
+    //console.log(`No query! Sending to controller`.red)
     next();
   } else {
-    console.log(`After query check`.blue)
-    console.log(`model.modelName`.blue, model.modelName)
+    //console.log(`After query check`.blue)
+    //console.log(`model.modelName`.blue, model.modelName)
     let query;
 
     // Making a copy of req.query 
     const reqQuery = { ...req.query };
-    console.log('advancedResults reqQuery: '.red, reqQuery)
+    //console.log('advancedResults reqQuery: '.red, reqQuery)
 
     // looking for populates/nest in the query
-    console.log('adv.R.M.W. req.query.populate '.red, req.query.populate)
+    //console.log('advancedResults req.query.populate '.red, req.query.populate)
     if (req.query.populate) {
       if (req.query.popselect) {
         let fields = req.query.popselect.split(',').join(' ');
-        console.log('adv.R.M.W popselect -> fields'.yellow, fields)
+        //console.log('advancedResultspopselect -> fields'.yellow, fields)
         populate = { path: req.query.populate, select: fields }
-        console.log('adv.Mw populate', populate);
+        //console.log('adv.Mw populate', populate);
       } else {
         populate = { path: req.query.populate }
-        console.log('adv.Mw populate', populate);
+        //console.log('adv.Mw populate', populate);
       }
 
     }
@@ -65,12 +65,12 @@ const advancedResults = (model, populate) => async (req, res, next) => {
 
     // Loop over removeFields and delete them from reqQuery if it has them
     removeFields.forEach(param => delete reqQuery[param]);
-    console.log('select/sort/page/limit removed reqQuery: ', reqQuery);
+    //console.log('select/sort/page/limit removed reqQuery: ', reqQuery);
 
 
     // check for geocode parameters and add location query object: 
     if (req.params.zipcode && req.params.distance) {
-      console.log('advancedResults.js, req.params.zipcode'.america, req.params.zipcode, 'req.params.distance: ', req.params.distance)
+      //console.log('advancedResults.js, req.params.zipcode'.america, req.params.zipcode, 'req.params.distance: ', req.params.distance)
       const { zipcode, distance } = req.params;
 
       // Get lat/lng from geocoder
@@ -83,7 +83,7 @@ const advancedResults = (model, populate) => async (req, res, next) => {
       const radius = distance / 3963;
       reqQuery.location = { $geoWithin: { $centerSphere: [[lng, lat], radius] } }
 
-      console.log('advancedResults.js added location:  '.yellow.bg, reqQuery)
+      //console.log('advancedResults.js added location:  '.yellow.bg, reqQuery)
     }
     // if client wants a single object, vendor or product, or all the images of a vendor or product. 
     if (req.params.vendorId) {
@@ -93,7 +93,7 @@ const advancedResults = (model, populate) => async (req, res, next) => {
         reference = 'vendor';
       } else reference = '_id';
       reqQuery[reference] = req.params.vendorId;
-      console.log('reference', reference);
+      //console.log('reference', reference);
     }
     if (req.params.productId) {
       let reference;
@@ -107,10 +107,10 @@ const advancedResults = (model, populate) => async (req, res, next) => {
     }
 
     let queryStr = JSON.stringify(reqQuery);
-    console.log(`advancedResults queryStr: `.yellow, queryStr);
-    console.log('model'.yellow, model.modelName)
+    //console.log(`advancedResults queryStr: `.yellow, queryStr);
+    //console.log('model'.yellow, model.modelName)
     queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
-    console.log(`advancedResults queryStr.replace `.green, queryStr);
+    //console.log(`advancedResults queryStr.replace `.green, queryStr);
     // Finding the resource. . 
     query = model.find(JSON.parse(queryStr));
 
@@ -118,7 +118,7 @@ const advancedResults = (model, populate) => async (req, res, next) => {
     if (req.query.select) {
 
       let fields = req.query.select.split(',').join(' ');
-      console.log('advancedResults select fields'.blue, fields);
+      //console.log('advancedResults select fields'.blue, fields);
       // note: with select you cannot select and deselect fields at the same time. 
       query = query.select(fields);
     }
@@ -142,7 +142,7 @@ const advancedResults = (model, populate) => async (req, res, next) => {
     const total = await model.countDocuments();
 
     query = query.skip(startIndex).limit(limit);
-    console.log('Populate'.red, populate)
+    //console.log('Populate'.red, populate)
     if (populate) {
       query = query.populate(populate);
       populate = "";
